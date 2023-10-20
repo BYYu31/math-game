7083580fbb9f5ca70ae2108cd41d3eafc1bf6aeb
Bo Yang Yu <boyangyu93@gmail.com>
2023-10-19 23:33:19 -0400

    create accessor for all instance variables

--- game.rb
+++ game.rb
@@ -2,6 +2,7 @@ require './player'
 require './question'
 
 class Game
+  attr_accessor :current_player, :player1, :player2
   def initialize
     @player1 = Player.new("Player 1")
     @player2 = Player.new("Player 2")
@@ -9,20 +10,20 @@ class Game
   end 
 
   def start
-    puts "Are you ready #{@player1.name} and #{@player2.name}? \n Let's start the game!"
+    puts "Are you ready #{player1.name} and #{player2.name}? \n Let's start the game!"
     new_round
   end
 
   def new_round
     new_q = Question.new
-    @new_question = new_q.question
-    @new_answer = new_q.answer
+    new_question = new_q.question
+    new_answer = new_q.answer
 
-    puts "Player #{@current_player} asks: #{@new_question}"
+    puts "Player #{current_player} asks: #{new_question}"
     print ">"
     input = $stdin.gets.chomp
-    player_to_answer = @current_player == 1 ? 2 : 1
-    true_or_false(input.to_i, @new_answer, player_to_answer)
+    player_to_answer = current_player == 1 ? 2 : 1
+    true_or_false(input.to_i, new_answer, player_to_answer)
     update_score
 
     puts '------ NEW TURN ------'
@@ -35,21 +36,21 @@ class Game
     else
       puts "Well, that was an attempt"
       if player == 1
-        @player1.health -= 1
+        player1.health -= 1
       else
-        @player2.health -= 1
+        player2.health -= 1
       end
     end
   end
 
   def update_score
-    if @player1.health > 0 && @player2.health > 0
-      puts "Player1: #{@player1.health}/3 vs Player2: #{@player2.health}/3"
+    if player1.health > 0 && player2.health > 0
+      puts "Player1: #{player1.health}/3 vs Player2: #{player2.health}/3"
     else
-      if @player1.health == 0
-        puts "Player 2 wins with a score of #{@player2.health}"
+      if player1.health == 0
+        puts "Player 2 wins with a score of #{player2.health}"
       else 
-        puts "Player 1 wins with a score of #{@player1.health}"
+        puts "Player 1 wins with a score of #{player1.health}"
       end
       puts '------ GAME OVER ------'
       puts 'Bye bye now!'
@@ -58,10 +59,10 @@ class Game
   end
 
   def change_current_player
-    if @current_player == 1
-      @current_player = 2
+    if current_player == 1
+      current_player = 2
     else
-      @current_player = 1
+      current_player = 1
     end
     new_round
   end
